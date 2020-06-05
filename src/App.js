4caf2d30727b988cb0e384e09325ce0219fc73fb
ABRAR-E-GOHAR <gohar.abrar5@gmail.com>
2020-06-05 11:58:57 +0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ABRAR E GOHAR
         </a>
       </header>
     </div>
